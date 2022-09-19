b59b0ef0e996964bc60f0068c611cb0cd2fb6933
chuan322 <phengchhoung@gmail.com>
2022-09-20 00:23:23 +0700

    Update about.js

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -140,22 +140,10 @@ const About = () => {
 
             <p>
               Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
+              <a href="https://us.mullenlowe.com/">NSSF</a>,{' '}
+              <a href="https://starry.com/">Coolbeans Digital</a>,{' '}. My
+              main focus these days is building iOS applications with beautiful designs <a href="https://coolbeans.digital">Coolbeans Studio</a> for a variety of
               clients.
-            </p>
-
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
