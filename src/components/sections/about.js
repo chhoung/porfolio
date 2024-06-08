76178a214cacc5ee87a7b2b20c107fefbefe768b
chhorng <phengchhorng@gmail.com>
2024-06-08 19:14:06 +1000

    fix: update url to assets

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -157,7 +157,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/me1.jpg"
+              src="../../images/me.jpg"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
